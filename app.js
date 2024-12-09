2b54ed0778fa52121319e53b9cb71df5fa1413f9
Sergei Egorov <75026810+EgorovSV@users.noreply.github.com>
2024-12-09 23:08:34 +0400

    First version

---
+++ app.js
@@ -0,0 +1,48 @@
+function runLotto() {
+  var body = document.getElementsByTagName("body")[0];
+  var table = document.createElement("table");
+  table.style.width = "100%";
+  table.setAttribute("border", "0");
+  var tbdy = document.createElement("tbody");
+
+  for (var i = 0; i < 2; i++) {
+    var tr = document.createElement("tr");
+    for (var j = 0; j < 2; j++) {
+      var td = document.createElement("td");
+      td.appendChild(createTabel());
+      tr.appendChild(td);
+    }
+    tbdy.appendChild(tr);
+  }
+  body.appendChild(table);
+  table.appendChild(tbdy);
+}
+
+function createTabel() {
+  const header = ["B", "I", "N", "G", "O"];
+  var table = document.createElement("table");
+  var tbdy = document.createElement("tbody");
+
+  const numbers = [];
+  for (var i = 0; i < 5; i++) {
+    var tr = document.createElement("tr");
+    for (var j = 0; j < 5; j++) {
+      if (i == 0) {
+        var th = document.createElement("th");
+        th.appendChild(document.createTextNode(header[j]));
+        tr.appendChild(th);
+      } else {
+        var td = document.createElement("td");
+        do {
+          var number = Math.ceil(Math.random() * 14) + 15 * j + 1;
+        } while (numbers.indexOf(number) != -1);
+        td.appendChild(document.createTextNode(number));
+        numbers.push(number);
+        tr.appendChild(td);
+      }
+    }
+    tbdy.appendChild(tr);
+  }
+  table.appendChild(tbdy);
+  return table;
+}
